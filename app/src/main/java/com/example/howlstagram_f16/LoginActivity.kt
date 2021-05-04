6366e8d9eb823ffdfd8d13af25a5d4c03fdb259a
hansungbin <happynewmind@gmail.com>
2021-05-04 23:42:26 +0900

    4강 1:40 hash key

--- app/src/main/java/com/example/howlstagram_f16/LoginActivity.kt
+++ app/src/main/java/com/example/howlstagram_f16/LoginActivity.kt
@@ -4,12 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     var auth : FirebaseAuth? = null
+    var googleSignInClient : GoogleSignInClient? = null
+    var GOOGLE_LOGIN_CODE = 9001
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -17,6 +26,48 @@ class LoginActivity : AppCompatActivity() {
         email_login_button.setOnClickListener {
             signinAndSignup()
         }
+        google_sign_in_button.setOnClickListener {
+            //First step
+            googleLogin()
+        }
+
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("697336115550-5c2o53v0j00l10j3ebld7pd4vkpn6a9v.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+    }
+
+    fun googleLogin(){
+        var signInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == GOOGLE_LOGIN_CODE){
+            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if(result!!.isSuccess){
+                var account = result.signInAccount
+                //Second step
+                firebaseAuthWithGoogle(account)
+            }
+        }
+    }
+
+    fun firebaseAuthWithGoogle (account: GoogleSignInAccount?){
+        var credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener {
+                    task ->
+                if(task.isSuccessful){
+                    //Login
+                    moveMainPage(task.result?.user)
+                }else {
+                    //Show the error message
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                }
+            }
     }
 
     fun signinAndSignup(){
